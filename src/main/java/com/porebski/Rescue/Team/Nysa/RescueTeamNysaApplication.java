0839999002b534d7812ebf341e23345195142e8f
kaelg <bartekxk@gmail.com>
2023-05-16 23:33:29 +0200

    Initial commit

---
+++ src/main/java/com/porebski/Rescue/Team/Nysa/RescueTeamNysaApplication.java
@@ -0,0 +1,12 @@
+package com.porebski.Rescue.Team.Nysa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescueTeamNysaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RescueTeamNysaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/porebski/Rescue/Team/Nysa/RescueTeamNysaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porebski.Rescue.Team.Nysa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescueTeamNysaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
